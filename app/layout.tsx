bb51ab96f7096bf7a0be98321799de9f48820561
Your Name <your-email@example.com>
2025-04-03 16:17:43 +0800

    feat: add Google Analytics tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Q3LSNTRHP7"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Q3LSNTRHP7');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className} min-h-screen bg-[#f0f7ff]`}>
         <Navbar />
         <main>{children}</main>
